0f705c30ff6c63337f6e4eac51c4542bc67c7aa0
Masaaki618 <masaaki_618@icloud.com>
2026-04-30 08:22:31 +0900

    完成

---
+++ paiza/array_utilization_primer__count10.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var (
+	r = bufio.NewReader(os.Stdin)
+	w = bufio.NewWriter(os.Stdout)
+)
+
+func main() {
+	defer w.Flush()
+	var x, y int
+	fmt.Fscan(r, &x, &y)
+	fmt.Fprintln(w, y, x)
+}
