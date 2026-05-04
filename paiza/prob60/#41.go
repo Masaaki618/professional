e76e3da3788c4c9998f69289c80b5defbc7ddfe6
Masaaki618 <masaaki_618@icloud.com>
2026-05-04 16:01:51 +0900

    完成

---
+++ paiza/prob60/#41.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var (
+	r = bufio.NewReader(os.Stdin)
+	w = bufio.NewWriter(os.Stdout)
+)
+
+func main() {
+	defer w.Flush()
+	var x int
+	fmt.Fscan(r, &x)
+	fmt.Fprintln(w, x+1)
+}
