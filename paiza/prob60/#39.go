222c00b89a8ba52e7606762481bd8193899f0051
Masaaki618 <masaaki_618@icloud.com>
2026-05-04 15:05:05 +0900

    完成

---
+++ paiza/prob60/#39.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"sort"
+)
+
+var (
+	r = bufio.NewReader(os.Stdin)
+	w = bufio.NewWriter(os.Stdout)
+)
+
+func main() {
+	defer w.Flush()
+	var x int
+	fmt.Fscan(r, &x)
+
+	arr := make([]int, x)
+
+	for i := 0; i < x; i++ {
+		fmt.Fscan(r, &arr[i])
+	}
+
+	sort.Ints(arr)
+	for _, v := range arr {
+		fmt.Fprintln(w, v)
+	}
+}
