e449c7242bf8765f6a36133e2a107c4449ab2294
Masaaki618 <masaaki_618@icloud.com>
2026-05-09 14:20:51 +0900

    完成

---
+++ paiza/class_primer/#8.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+var (
+	r = bufio.NewReader(os.Stdin)
+	w = bufio.NewWriter(os.Stdout)
+)
+
+type Point struct {
+	alpha string
+	road1 int
+	road2 int
+}
+
+func main() {
+	defer w.Flush()
+	var N, K, S int
+	fmt.Fscan(r, &N, &K, &S)
+
+	points := make(map[int]Point, N)
+	for i := 1; i <= N; i++ {
+		var alpha string
+		var road1, road2 int
+		fmt.Fscan(r, &alpha, &road1, &road2)
+		points[i] = Point{alpha, road1, road2}
+	}
+
+	fmt.Fprintln(w, points)
+
+	current := S
+	fmt.Fprintf(w, points[current].alpha)
+
+	for i := 0; i < K; i++ {
+		var m int
+		fmt.Fscan(r, &m)
+		if m == 1 {
+			current = points[current].road1
+		} else {
+			current = points[current].road2
+		}
+		fmt.Fprintf(w, points[current].alpha)
+	}
+	fmt.Fprintln(w)
+}
