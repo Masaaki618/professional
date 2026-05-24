00c24ddeb75e731e30e570aae7bc5d4189744908
Masaaki618 <masaaki_618@icloud.com>
2026-05-24 21:40:14 +0900

    リファクタ

--- paiza/b_rank_new_level_up_problems/#1.go
+++ paiza/b_rank_new_level_up_problems/#1.go
@@ -15,25 +15,19 @@ func main() {
 	defer w.Flush()
 	var H, W, y, x int
 	fmt.Fscan(r, &H, &W)
-	list := make([]string, H)
+	board := make([][]byte, H)
 	for i := 0; i < H; i++ {
 		var s string
 		fmt.Fscan(r, &s)
-		list[i] = s
+		board[i] = []byte(s)
 	}
-	fmt.Fscan(r, &x, &y)
-	// 文字列
-	listRow := list[x]
-	listByte := []byte(listRow)
-	if listByte[y] == '.' {
-		listByte[y] = '#'
+	fmt.Fscan(r, &y, &x)
+	if board[y][x] == '.' {
+		board[y][x] = '#'
 	} else {
-		listByte[y] = '.'
+		board[y][x] = '.'
 	}
-	list[x] = string(listByte)
-
-	for _, v := range list {
-		fmt.Fprintln(w, v)
-
+	for _, row := range board {
+		fmt.Fprintln(w, string(row))
 	}
 }
